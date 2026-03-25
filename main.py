fb583d0f2fff597edf835ee2e03a81a4abc87a63
dp-cross-projects <l.d.parra@outlook.com>
2026-03-24 21:47:25 -0300

    finished functionality

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-from fastapi import FastAPI, HTTPException, Request, Form, status
+# FastAPI Imports
+from fastapi import FastAPI, Request, Form, status
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse, RedirectResponse
+
+# Firebase Imports
 import firebase_admin
 from firebase_admin import credentials, firestore
 from google.cloud.firestore_v1.base_query import FieldFilter
 
-from pydantic import BaseModel
+# Other Imports
 from typing import Literal, Annotated
-
 from operator import itemgetter
 
-## Init firestore database
+## Init Firestore database
 cred = credentials.Certificate(".\\db_account.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
@@ -19,39 +21,47 @@ db = firestore.client()
 ## Init FastAPI
 app = FastAPI()
 
+# Mount Static folder
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+# Init Jinja 2 Templates
 templates = Jinja2Templates(directory="templates")
 
-# Pydantic
 
-class Transaction(BaseModel):
-    amount: float
-    category: str
-    date: str
-    type: Literal["income","expense"]
-
-## Collection names
+## Collections
 ACCOUNT_COLLECTION = db.collection("accounts")
-TRANSFER_COLLECTION = db.collection("transfers")
-TRANSACTION_COLLECTION = db.collection("transactions")
 CATEGORY_COLLECTION = db.collection("category")
 
 
-## Routes
-## HOME
+##### Routes #####
+
+## HOME ROUTES
 @app.get("/", response_class=HTMLResponse)
-async def execute_root(request: Request):
-    categories = get_categories()
+async def execute_root_expense(request: Request):
+    categories = get_categories("expense")
     accounts = get_accounts()
-    # incomes = 
     return templates.TemplateResponse(
         request=request, name="index.html", context={
             "categories":categories,
-            "accounts": accounts["accounts"]
+            "accounts": accounts["accounts"],
+            "transaction_type": "expense"
         }
     )
 
+@app.get("/add-income", response_class=HTMLResponse)
+async def execute_root_income(request: Request):
+    categories = get_categories("income")
+    accounts = get_accounts()
+    return templates.TemplateResponse(
+        request=request, name="index.html", context={
+            "categories":categories,
+            "accounts": accounts["accounts"],
+            "transaction_type": "income"
+        }
+    )
+
+
+## TRANSACTION ROUTES
 @app.get("/transaction/{account_id}/{transaction_type}/{transaction_id}", response_class=HTMLResponse)
 async def render_transaction_details(request: Request, account_id, transaction_type, transaction_id):
     doc = ACCOUNT_COLLECTION.document(account_id).collection(transaction_type).document(transaction_id)
@@ -94,7 +104,7 @@ def create_new_transaction(
         new_balance = balance - amount
         ACCOUNT_COLLECTION.document(account).set({"balance": new_balance})
 
-    redirect_url = request.url_for("execute_root")
+    redirect_url = request.url_for("execute_root_expense")
     return RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
 
 @app.post("/transaction/update/{account_id}/{transaction_type}/{transaction_id}")
@@ -147,18 +157,11 @@ def delete_transaction(request: Request, account_id,transaction_type,transaction
     redirect_url = f"/account/{account_id}"
     return RedirectResponse(url=redirect_url, status_code=status.HTTP_303_SEE_OTHER)
 
-#### END HOME ######
-
-## CATEGORY
+## CATEGORY ROUTES
 @app.get("/category", response_class=HTMLResponse)
 async def render_category(request: Request):
-    # documents = CATEGORY_COLLECTION.get()
-    # collections = []
-    # for doc in documents:
-    #     collections.append(doc.id)
     income_categories = get_categories("income")
     expense_categories = get_categories("expense")
-    
 
     return templates.TemplateResponse(
         request=request, name="category.html", context={"income_categories": income_categories, "expense_categories": expense_categories}
@@ -183,19 +186,10 @@ def add_new_category(
     redirect_url = request.url_for("render_category")
     return RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
     
-#### END CATEGORY ######
-
-## ACCOUNT
+## ACCOUNT ROUTES
 
 @app.get("/account", response_class=HTMLResponse)
 async def render_account(request: Request):
-    # documents = ACCOUNT_COLLECTION.get()
-    # collections = []
-    # total_balance = 0
-
-    # for doc in documents:
-    #     collections.append({"id": doc.id, "balance":doc.to_dict()["balance"]})
-    #     total_balance += doc.to_dict()["balance"]
     accounts = get_accounts()
     
     return templates.TemplateResponse(
@@ -209,14 +203,6 @@ async def render_account_details(request: Request, account_id):
 
     income = sorted(get_transactions(documents, "income"), key=itemgetter("date"), reverse=True)
     expenses = sorted(get_transactions(documents, "expense"), key=itemgetter("date"), reverse=True)
-    
-    # collections = []
-    # total_balance = 0
-
-    # for doc in documents:
-    #     collections.append({"id": doc.id, "balance":doc.to_dict()["balance"]})
-    #     total_balance += doc.to_dict()["balance"]
-    # accounts = get_accounts()
     
     return templates.TemplateResponse(
         request=request,
@@ -246,7 +232,7 @@ def add_new_account(request: Request, account: Annotated[str, Form()]):
     
 
 
-#### END ACCOUNT ######
+##### Utilities #####
 
 def get_transactions(account, type):
     data = account.collection(type).get()
